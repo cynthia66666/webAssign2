67f01cb3944eeeb6249cabd55813bd6d37e90ca3
Cynthia Huang <cynthiahuanglulu@gmail.com>
2020-03-03 18:07:21 -0500

    login validation

--- server.js
+++ server.js
@@ -132,8 +132,29 @@ app.post("/cusRegistration",(req,res)=>{
 
 
 app.post("/login",(req,res)=>{
-   
-    
+   const errornm=[];
+   const errorpw=[];
+   if(req.body.signinEmail=="")
+   {
+       errornm.push("Please enter your email");
+   }
+   if(req.body.signinPassword=="")
+   {
+       errorpw.push("Please enter your password");
+   }
+   if(errornm.length>0||errorpw.length>0)
+   {
+       res.render("login",{
+           title: "Login Page",
+           nmerror:errornm,
+           pwerror:errorpw,
+           signinEmail:req.body.signinEmail,
+           password:req.body.signinPassword
+       });
+   }
+   else{
+       res.redirect("/products");
+   }
 });
 const PORT = 3000;
 app.listen(PORT,()=>{
