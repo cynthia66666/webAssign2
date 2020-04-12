f0c31b6da66a642ba32b7fadc4bd53650153260f
Cynthia Huang <cynthiahuanglulu@gmail.com>
2020-04-11 22:38:13 -0400

    port

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ mongoose.connect(process.env.MANGO_DB_CONNECTION_STRING, {useNewUrlParser: true,
 })
 .catch(err=>console.log(`ERROR occured when connecting to the database ${err}`))
 
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
+//const PORT=;
+app.listen(process.env.PORT||3000,()=>{
     console.log(`Web Server Started`);
 });
