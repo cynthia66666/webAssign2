543f2ecfda7b81febf53c6f743e6fc203cfe0be5
Cynthia Huang <cynthiahuanglulu@gmail.com>
2020-04-17 02:19:45 -0400

    pulling data

--- controllers/product.js
+++ controllers/product.js
@@ -15,20 +15,50 @@ router.post("/add",(req,res)=>
         category : req.body.category,
         quantity : req.body.quantity,
         bestSeller : req.body.bestSeller,
+        status:req.body.status
     }
 
     const product = new productModel(newProduct);
     product.save()
     .then(()=>{
        
-                res.redirect(`/product/list`);
+                res.redirect('/product/list');
             })
     .catch(err=>console.log(`Error happened when inserting in the database:${err}`));
    
 });
 
 router.get("/list",(req,res)=>{
-    res.render("Product/productDashboard");
+    //pull from the database, get the results that was returned and 
+    //then inject that results into the productDashboard
+    productModel.find()
+    .then((products)=>{
+        //Filter out the information that you want from the arrary
+        //of documents that was returned into a new array
+
+        //the call up function will be executed as many times as
+        //how many arrarys of documents there
+        const filteredProduct=products.map(product=>{
+            return {
+                id:product._id,
+                name:product.name,
+                price:product.price,
+                description:product.description,
+                category:product.category,
+                quantity:product.quantity,
+                bestSeller:product.bestSeller,
+                status:product.status,
+                productImg:product.productImg
+            }
+        });
+
+
+        res.render("Product/productDashboard",{
+            data:filteredProduct
+        });
+    })
+    .catch(err=>console.log(`Error happened when pulling from the database:${err}`));
+
 });
 
 
